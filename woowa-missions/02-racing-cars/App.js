d75af5fac58db77887ca6061fc4f475986a76381
yshls <shluxnsal01@gmail.com>
2025-12-04 00:13:51 +0900

    다시 한번 더

--- woowa-missions/02-racing-cars/App.js
+++ woowa-missions/02-racing-cars/App.js
@@ -1,46 +1,62 @@
+/**
+class App {
+  async run() {
+    // 1. 사용자한테 차 이름 입력받기
+    // 2. 쉼표로 쪼개서 배열로 만들기
+    // 3. 차 이름마다 { 이름, 위치: 0 } 인 객체로 변환하기 (여기서 5글자 체크도 해야 함)
+    
+    // 4. 시도할 횟수 입력받기 (숫자로 바꾸기)
+    
+    // 5. 횟수만큼 반복하기 (Game Loop)
+        // 5-1. 모든 차를 한 번씩 돌면서 주사위 굴리기
+        // 5-2. 4 이상이면 전진시키기
+        // 5-3. 현재 상태 출력하기 (별 찍기)
+    
+    // 6. 제일 멀리 간 거리 구하기
+    // 7. 그 거리만큼 간 차들 이름만 뽑기
+    // 8. 우승자 출력하기
+  }
+}
+*/
+
 import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      // 1. 입력 및 검증
-      const answer = await MissionUtils.Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      // 1. 사용자한테 차 이름 입력받기
+      const carName = await MissionUtils.Console.readLineAsync(
+        '레이싱할 자동차 이름을 입력해주세요.'
       );
-      const carNames = answer.split(',');
-      // 2. 자동차 객체 생성
-      const cars = carNames.map((name) => {
+      // 2. 쉼표로 쪼개서 배열로 만들기
+      const carNameSplit = carName.split(',');
+      // 3. 차 이름마다 { 이름, 위치: 0 } 인 객체로 변환하기
+      const cars = carNameSplit.map((name) => {
         if (name.length > 5) {
-          throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+          throw new Error('[ERROR]');
         }
-        // 각 자동차의 상태(이름, 위치)를 초기화해서 반환
         return { name: name, position: 0 };
       });
-      // 2. 시도 횟수 입력
-      const input = await MissionUtils.Console.readLineAsync('시도할 횟수:');
-      const tryCount = Number(input);
-      // 숫자가 이닐 경우 에러처리
-      if (isNaN(tryCount)) {
-        throw new Error('[ERROR] 숫자를 입력해 주세요.');
-      }
-      // 3. 게임 시작 메시지
-      MissionUtils.Console.print('\n실행 결과');
-      // 시도할 횟수
-      for (let i = 0; i < tryCount; i++) {
-        // 각 턴마다 모든 자동차를 하나씩 검사
+      // 4. 시도할 횟수 입력받기 (숫자로 바꾸기)
+      let input = await MissionUtils.Console.readLineAsync(
+        '시도할 횟수를 입력해주세요.'
+      );
+
+      let numInput = Number(input);
+
+      // 횟수 만큼 반복하기
+      for (let i = 0; i < numInput; i++) {
         cars.forEach((car) => {
           const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
 
-          // 4이상일 경우 전진
           if (randomNum >= 4) {
             car.position += 1;
           }
 
-          // 전진 결과 출력(화면 그리기)
           MissionUtils.Console.print(
             `${car.name} : ${'-'.repeat(car.position)}`
           );
         });
-        // 줄바꿈
+
         MissionUtils.Console.print('');
       }
 
@@ -49,7 +65,6 @@ class App {
       const winners = cars
         .filter((car) => car.position === maxPosition)
         .map((car) => car.name);
-
       MissionUtils.Console.print(`최종 우승자 : ${winners.join(', ')}`);
     } catch (error) {
       MissionUtils.Console.print(error.message);
